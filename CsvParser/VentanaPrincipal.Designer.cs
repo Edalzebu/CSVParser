8406e3b97449f6c91a382a7217939a4471464ae7
Edwin Zelaya <edwin_zelaya5@unitec.edu>
2014-03-30 23:22:06 -0600

    Primer commit

---
+++ CsvParser/VentanaPrincipal.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace CsvParser
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.listaCampos = new System.Windows.Forms.ListBox();
+            this.tablaGrid = new System.Windows.Forms.DataGridView();
+            this.openFileWindow = new System.Windows.Forms.OpenFileDialog();
+            this.btnAgregarCampo = new System.Windows.Forms.Button();
+            this.txtBoxAgregarCampo = new System.Windows.Forms.TextBox();
+            this.btnPasarColumna = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Enabled = false;
+            this.btnImportar.Location = new System.Drawing.Point(905, 60);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(75, 23);
+            this.btnImportar.TabIndex = 0;
+            this.btnImportar.Text = "Importar";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // listaCampos
+            // 
+            this.listaCampos.FormattingEnabled = true;
+            this.listaCampos.Location = new System.Drawing.Point(12, 145);
+            this.listaCampos.Name = "listaCampos";
+            this.listaCampos.Size = new System.Drawing.Size(167, 329);
+            this.listaCampos.TabIndex = 1;
+            // 
+            // tablaGrid
+            // 
+            this.tablaGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaGrid.Location = new System.Drawing.Point(224, 145);
+            this.tablaGrid.Name = "tablaGrid";
+            this.tablaGrid.Size = new System.Drawing.Size(756, 329);
+            this.tablaGrid.TabIndex = 2;
+            this.tablaGrid.ColumnHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.tablaGrid_ColumnHeaderMouseDoubleClick);
+            // 
+            // openFileWindow
+            // 
+            this.openFileWindow.FileName = "openFileWindow";
+            // 
+            // btnAgregarCampo
+            // 
+            this.btnAgregarCampo.Location = new System.Drawing.Point(181, 114);
+            this.btnAgregarCampo.Name = "btnAgregarCampo";
+            this.btnAgregarCampo.Size = new System.Drawing.Size(26, 23);
+            this.btnAgregarCampo.TabIndex = 3;
+            this.btnAgregarCampo.Text = "+";
+            this.btnAgregarCampo.UseVisualStyleBackColor = true;
+            this.btnAgregarCampo.Click += new System.EventHandler(this.btnAgregarCampo_Click);
+            // 
+            // txtBoxAgregarCampo
+            // 
+            this.txtBoxAgregarCampo.Location = new System.Drawing.Point(12, 116);
+            this.txtBoxAgregarCampo.Name = "txtBoxAgregarCampo";
+            this.txtBoxAgregarCampo.Size = new System.Drawing.Size(167, 20);
+            this.txtBoxAgregarCampo.TabIndex = 4;
+            // 
+            // btnPasarColumna
+            // 
+            this.btnPasarColumna.Location = new System.Drawing.Point(185, 284);
+            this.btnPasarColumna.Name = "btnPasarColumna";
+            this.btnPasarColumna.Size = new System.Drawing.Size(22, 23);
+            this.btnPasarColumna.TabIndex = 5;
+            this.btnPasarColumna.Text = ">";
+            this.btnPasarColumna.UseVisualStyleBackColor = true;
+            this.btnPasarColumna.Click += new System.EventHandler(this.btnPasarColumna_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(905, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 506);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnPasarColumna);
+            this.Controls.Add(this.txtBoxAgregarCampo);
+            this.Controls.Add(this.btnAgregarCampo);
+            this.Controls.Add(this.tablaGrid);
+            this.Controls.Add(this.listaCampos);
+            this.Controls.Add(this.btnImportar);
+            this.Name = "VentanaPrincipal";
+            this.Text = "VentanaPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.tablaGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.ListBox listaCampos;
+        private System.Windows.Forms.DataGridView tablaGrid;
+        private System.Windows.Forms.OpenFileDialog openFileWindow;
+        private System.Windows.Forms.Button btnAgregarCampo;
+        private System.Windows.Forms.TextBox txtBoxAgregarCampo;
+        private System.Windows.Forms.Button btnPasarColumna;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ CsvParser/VentanaPrincipal.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvParser
+{
+    public partial class VentanaPrincipal : Form
+    {
+        DataTable dt = new DataTable();
+        
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+            this.listaCampos.MouseDoubleClick += new MouseEventHandler(listaCampos_MouseDoubleClick);
+        }
+
+        private void listaCampos_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listaCampos.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                PasardeListadeCamposaColumna();
+            }
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            var path = GetPathofFileAndOpenDialog();
+            if (!CheckIfItIsaValidPath(path)) return;
+            var allRegisters = GetWholeRegistersFromFile(path);
+            var listaDataCampos = GetListDataFromNormalCsvRegister(allRegisters);
+
+            var lista = new List<string>();
+            foreach (var dataCampo in listaDataCampos)
+            {
+                lista.Add(dataCampo);
+                if (lista.Count != dt.Columns.Count) continue;
+                dt.Rows.Add(lista.ToArray());
+                lista.Clear();
+            }
+            
+            tablaGrid.DataSource = dt;
+            //foreach (var registro in listaRegistros)
+            //{
+            //    var listaDataPorRegistro = GetListDataFromNormalCsvRegister(registro);
+            //    for (var i = 0; i < tablaGrid.ColumnCount; i++)
+            //    {
+            //        var row = (DataGridViewRow)tablaGrid.Rows[i];
+            //        row.Cells[i].Value = listaDataPorRegistro.ElementAt(i);
+            //        tablaGrid.Rows.Add(row);
+            //    }
+            //}
+        }
+
+        protected List<string> GetListDataFromNormalCsvRegister(string register)
+        {
+            return register.Split(',').ToList();
+        }
+        protected List<string> GetRegistersFromFile(string path)
+        {
+            return System.IO.File.ReadAllLines(path).ToList();
+        }
+
+        protected string GetWholeRegistersFromFile(string path)
+        {
+           return System.IO.File.ReadAllText(path);
+        }
+        protected string GetPathofFileAndOpenDialog()
+        {
+            var path ="";
+            if (openFileWindow.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileWindow.FileName;
+            }
+            return path;
+        }
+
+        private static bool CheckIfItIsaValidPath(string path)
+        {
+            return !string.IsNullOrEmpty(path);
+        }
+
+        private void btnAgregarCampo_Click(object sender, EventArgs e)
+        {
+            if (txtBoxAgregarCampo.Text == "") return;
+            listaCampos.Items.Add(txtBoxAgregarCampo.Text);
+            btnImportar.Enabled = true;
+            txtBoxAgregarCampo.Clear();
+        }
+
+        private void btnPasarColumna_Click(object sender, EventArgs e)
+        {
+           PasardeListadeCamposaColumna();
+        }
+
+        private void tablaGrid_ColumnHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            var columnIndex = tablaGrid.CurrentCellAddress.X;
+            listaCampos.Items.Add(tablaGrid.Columns[columnIndex].Name);
+            dt.Columns.RemoveAt(columnIndex);
+            tablaGrid.DataSource = dt;
+        }
+
+        protected void PasardeListadeCamposaColumna()
+        {
+            var item = listaCampos.SelectedItem;
+            dt.Columns.Add(new DataColumn(item as string, typeof(string)));
+            tablaGrid.DataSource = dt;
+            listaCampos.Items.Remove(item);
+        }
+
+    }
+}
